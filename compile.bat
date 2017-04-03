8c1ecdd175a30f1990f098abf7c312c7b6188a58
Tzu-ting <tzing@outlook.com>
2017-04-03 15:31:08 +0800

    improve readability & modify compiling config

--- compile.bat
+++ compile.bat
@@ -1,24 +1,30 @@
 @echo off
 
 :: check argument
-if "%1" EQU "" goto err
-if "%2" EQU "" goto err
-
-:: start
-echo "compile %1 -> %2"
-
-for /f %%f in ('dir /b %1\*.c %1\*.cpp') do (
-    echo %%f
-    if "%3" NEQ "" (
-        cl.exe /O2 "%1\\%%f" > "%3\\%%f.txt"
-    )
-    else (
-        cl.exe /O2 "%1\\%%f"
-    )
+if "%1" NEQ "" (
+    set src=%1
+) else (
+    goto err
+)
+if "%2" NEQ "" (
+    set out=%2
+) else (
+    goto err
 )
 
-:: move
-mv *.exe %2
+:: setup environment
+call "%VS140COMNTOOLS%\\vsvars32.bat"
+
+:: looping
+for /f "tokens=1 delims=." %%f in ('dir /b %1\*.c %1\*.cpp') do (
+    echo %%f
+    if "%3" NEQ "" (
+        cl.exe /O2 /I=. /Fe:"%out%\\%%f.exe" "%src%\\%%f.cpp" > %3\\%%f.txt
+    )
+    else (
+        cl.exe /O2 /Fe:"%out%\\%%f.exe" "%src%\\%%f.cpp"
+    )
+)
 
 :: clean
 rm -f *.obj
